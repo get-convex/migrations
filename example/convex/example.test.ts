7c9b55ae7373e96b381e379400adb0a3dcce7f19
Ian Macartney <ianhmacartney@gmail.com>
2025-11-19 13:42:23 -0800

    add example test

---
+++ example/convex/example.test.ts
@@ -0,0 +1,34 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
+import { initConvexTest } from "./setup.test";
+import { internal } from "./_generated/api";
+import { migrations } from "./example";
+
+describe("example", () => {
+  beforeEach(async () => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(async () => {
+    vi.useRealTimers();
+  });
+
+  test("setDefaultValue", async () => {
+    const t = initConvexTest();
+    await t.mutation(internal.example.seed, { count: 10 });
+    await t.run(async (ctx) => {
+      const docs = await ctx.db.query("myTable").collect();
+      expect(docs).toHaveLength(10);
+      expect(docs.some((doc) => doc.optionalField === undefined)).toBe(true);
+      await migrations.runOne(ctx, internal.example.setDefaultValue, {
+        batchSize: 2,
+        dryRun: false,
+      });
+    });
+    await t.finishAllScheduledFunctions(vi.runAllTimers);
+    await t.run(async (ctx) => {
+      const after = await ctx.db.query("myTable").collect();
+      expect(after).toHaveLength(10);
+      expect(after.every((doc) => doc.optionalField !== undefined)).toBe(true);
+    });
+  });
+});
