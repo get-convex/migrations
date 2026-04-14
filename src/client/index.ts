307731a498a4010b99be60f78db9b138bb162ab8
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-04-14 00:51:33 -0700

    add stubs for syscalls

--- src/client/index.ts
+++ src/client/index.ts
@@ -714,3 +714,40 @@ type ActionCtx = Pick<
   GenericActionCtx<GenericDataModel>,
   "runQuery" | "runMutation" | "storage"
 >;
+
+// TODO: replace with ctx.meta.getFunctionMetadata() in 1.36+
+export async function getFunctionMetadata(): Promise<{
+  name: string;
+  componentPath: string;
+}> {
+  const syscalls = (global as any).Convex;
+  return JSON.parse(
+    await syscalls.asyncSyscall("1.0/getFunctionMetadata", JSON.stringify({})),
+  );
+}
+
+type TransactionMetric = {
+  used: number;
+  remaining: number;
+};
+
+type TransactionMetrics = {
+  bytesRead: TransactionMetric;
+  bytesWritten: TransactionMetric;
+  databaseQueries: TransactionMetric;
+  documentsRead: TransactionMetric;
+  documentsWritten: TransactionMetric;
+  functionsScheduled: TransactionMetric;
+  scheduledFunctionArgsBytes: TransactionMetric;
+};
+
+// TODO: replace with ctx.meta.getFunctionMetadata() in 1.36+
+export async function getTransactionMetrics(): Promise<TransactionMetrics> {
+  const syscalls = (global as any).Convex;
+  return JSON.parse(
+    await syscalls.asyncSyscall(
+      "1.0/getTransactionMetrics",
+      JSON.stringify({}),
+    ),
+  );
+}
